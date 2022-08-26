a9c9170ebb7002cca52a147cd6c575fc145102d3
osaguild <nobuhisa.abe.0922@gmail.com>
2022-08-26 09:09:44 +0900

    fix test apiKey to dummy

--- src/components/UploadButton/setMock.test.tsx
+++ src/components/UploadButton/setMock.test.tsx
@@ -2,26 +2,23 @@ import { render, screen } from '@testing-library/react'
 import { UploadButton } from '.'
 import { Metadata } from '../../lib/pinata'
 import '@testing-library/jest-dom'
-import 'dotenv/config'
 
 jest.mock('../../hooks/FileContext')
 jest.mock('../../lib/pinata')
 jest.setTimeout(10000)
-
-const PINATA_API_JWT = process.env.REACT_APP_PINATA_API_JWT as string
 
 describe('Upload()', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
   it('button is clickable', () => {
-    render(<UploadButton success={() => true} failed={() => true} pinataApiJwt={PINATA_API_JWT} />)
+    render(<UploadButton success={() => true} failed={() => true} pinataApiJwt="dummy" />)
     expect(screen.getByTestId('upload-button')).not.toBeDisabled()
   })
   it('click button', async () => {
     const success = jest.fn((metadata: Metadata) => metadata)
     const failed = jest.fn((message: string) => message)
-    render(<UploadButton success={success} failed={failed} pinataApiJwt={PINATA_API_JWT} />)
+    render(<UploadButton success={success} failed={failed} pinataApiJwt="dummy" />)
 
     screen.getByTestId('upload-button').click()
     await new Promise((resolve) => setTimeout(resolve, 1000))
