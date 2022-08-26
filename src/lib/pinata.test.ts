7eca04f9802a0e4926474a47d69baa8023bcaa39
osaguild <nobuhisa.abe.0922@gmail.com>
2022-08-26 14:11:16 +0900

    fix test image path

--- src/lib/pinata.test.ts
+++ src/lib/pinata.test.ts
@@ -8,7 +8,7 @@ const PINATA_API_JWT = process.env.REACT_APP_PINATA_API_JWT as string
 
 describe('upload()', () => {
   it('[success]', async () => {
-    const stream = fs.createReadStream('./src/file/sample.jpeg')
+    const stream = fs.createReadStream('./data/sample.jpeg')
     const res = await uploadFile('image_test', stream, PINATA_API_JWT)
     expect(res.PinSize).toBeGreaterThan(0)
   })
